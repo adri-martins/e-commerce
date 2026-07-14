34105f5908b053318c245e6b87717b07599ee05a
Adrieli Martins <martinsadrieli501@gmail.com>
2026-07-14 14:54:50 -0300

    feat(produto.ts): Criação de uma lista de produtos e no imports (colocamos  PrecoFormatadoPipe)

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import {UpperCasePipe, CurrencyPipe} from '@angular/common';
+import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe, PrecoFormatadoPipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
 export class Produto {
-  produto = 'Notebook';
-  preco = 2500;
-  mostrarPreco = true;
-  mostrarProduto = false;
+  //produto = 'Notebook';
+  //preco = 2500;
+  //mostrarPreco = true;
+  //mostrarProduto = false;
+ produtos = [
+  {produto:'Monitor',preco: 1000},
+  {produto: 'Teclado',preco: 600},
+  {produto:'Gabinete',preco: 1200},
+  {produto:'Mouse',preco: 350},
+  {produto:'Fone',preco: 650}
+ ];
 }
