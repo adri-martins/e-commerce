50e7384fac20941790cf412e7d1d6574d1a43507
Adrieli Martins <martinsadrieli501@gmail.com>
2026-07-15 13:40:20 -0300

    feat(lista-produtos.html): criação de uma teg, colocando nome e preco

--- src/app/components/lista-produtos/lista-produtos.ts
+++ src/app/components/lista-produtos/lista-produtos.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { Produto } from '../produto/produto';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [],
+  imports: [Produto],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
-export class ListaProdutos {}
+export class ListaProdutos {
+  produtos = [
+    {nome: 'Teclado', preco:49,}
+    {nome: 'Mouse', preco:670},
+    {nome: 'Cadeira Gamer', preco:900},
+    {nome: 'Monitor', preco:450}
+  ];
+}
