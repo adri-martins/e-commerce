431afb834a53b3e0e42ec4c9bac6a2815e5ce6bd
Adrieli Martins <martinsadrieli501@gmail.com>
2026-07-21 13:49:50 -0300

    refactor(app.ts): atualizar o nome da loja e importar RouterLink

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Produto } from './features/produtos/produto/produto';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { RouterOutlet, RouterLink } from '@angular/router';
+//import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
-  standalone: true,
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'ARM Infinity'; //NOME DO e-commerce
 }
-//adicionei import listaprodutos.14/07
 
