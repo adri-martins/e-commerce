d2c773439469d5864f2551df4454ace447b5426a
Adrieli Martins <martinsadrieli501@gmail.com>
2026-07-27 15:46:01 -0300

    ta

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { withInterceptors } from '@angular/common/http';
+import { httpInterceptorFn } from './core/interceptor/http.interceptor';
+
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -8,9 +13,10 @@ import { provideClientHydration } from '@angular/platform-browser';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), 
+    provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch())
-  ]
+    provideHttpClient(withFetch(),
+    withInterceptors([httpInterceptorFn])),
+  ],
 };
 //import provideHttpClient
