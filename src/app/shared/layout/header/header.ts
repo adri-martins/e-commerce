1b9332ccdf507b695aee79c308a2a4a985a65960
Adrieli Martins <martinsadrieli501@gmail.com>
2026-08-04 16:44:06 -0300

    feat(shared/layout): Cria pastas e arquivos dentro de shared

---
+++ src/app/shared/layout/header/header.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, RouterLink],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  nomeLoja = 'ARM Infinity'; //NOME DO e-commerce
+}
