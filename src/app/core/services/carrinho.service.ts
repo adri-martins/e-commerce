99c6002a192901729065bfe19984532f1b837b3a
Adrieli Martins <martinsadrieli501@gmail.com>
2026-08-06 15:00:59 -0300

    refactor estrutura com injectable

---
+++ src/app/core/services/carrinho.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { signal } from "@angular/core";
+import { computed } from "@angular/core";
+
+export class CarrinhoService {
+    //! Estado Global 
+private carrinho = signal<{nome: string; preco: number}[]>([]);
+
+//? Seletores
+itens = computed(() => this.carrinho());
+quantidadeItens= computed(() => this.carrinho().length); //!Quantidade de itens no carrinho
+totalItens = computed (() =>
+    this.carrinho().reduce((total, item) => total + item.preco,0)
+);
+
+// TODO/; Ações
+adicionar(produto: {nome: string; preco: number}){
+    this.carrinho.update(lista => [ ...lista, produto]);
+}
+
+limpar() {
+    this.carrinho.set([]);
+}
+}
