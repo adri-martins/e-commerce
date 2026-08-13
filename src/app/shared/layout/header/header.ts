832fb4376c1855b9e1abaac7fd15c82164d73308
Adrieli Martins <arthurcosta2727@gmail.com>
2026-08-13 16:56:50 -0300

    refactor: novo import

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,28 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { Router, RouterLink } from '@angular/router';
+import { inject } from '@angular/core';
 import { CarrinhoService } from '../../../core/services/carrinho.service';
 import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, RouterLink,],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-  nomeLoja = 'ARM Infinity'; //NOME DO e-commerce
+  nomeLoja = 'ARMInfinit'; //nome do e-commerce
 
   private CarrinhoService = inject(CarrinhoService);
-  quantidade = this.CarrinhoService.quantidadeItens;
   private authService = inject(AuthService);
+  private router = inject(Router)
   usuarioLogado = this.authService.usuarioLogado;
-  usuarioAtual = this.authService.usuarioAtual;
+  usuarioAtual = this.authService.usurioAtual;
+  quantidade = this.CarrinhoService.quantidadeItens;
+ 
 
-
-  sair(){
-    this.authService.logout();
-  }
+sair(){
+  this.authService.logout();
+  this.router.navigateByUrl('/login');
+}
 }
