9076b3c377ab6bafa1033b4e3d03f402802385ec
Adrieli Martins <arthurcosta2727@gmail.com>
2026-08-13 13:36:40 -0300

    refactor: novo importe e ajeita o codigo'

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { usuarioLogado, login, logout } from './core/auth';
 import { Header } from './shared/layout/header/header';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
   
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
+  
 }
 //apaguei uma palavra (fix)
