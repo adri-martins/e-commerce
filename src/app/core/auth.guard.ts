0bc1062755fe2a5d02e9fb4beb93bcf2014e0305
Adrieli Martins <arthurcosta2727@gmail.com>
2026-08-13 13:38:11 -0300

    feat: cria novo código para usuario autenticado

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,6 +1,16 @@
 import { CanActivateFn } from "@angular/router";
-import { usuarioLogado } from "./auth";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "./services/auth.service";
+
 export const authGuard: CanActivateFn = () => {
-   return usuarioLogado();
-};
+   const authService = inject(AuthService);
+   const router = inject(Router);
+
+   if(authService.usuarioLogado()){
+      return true;
+   }
+   return router.createUrlTree(['/login']);
+   
+}
 //SE O USUARIO TIVER AUTENTICADO O MESMO TERÁ ACESSO PARA A RORA
