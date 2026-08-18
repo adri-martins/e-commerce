6b8a26d00d6daf70e8bb79d02a142fe95f941cb8
Adrieli Martins <martinsadrieli501@gmail.com>
2026-08-18 17:19:02 -0300

    feat(carrinho.ts): novo import

--- src/app/features/carrinho/carrinho/carrinho.ts
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject} from '@angular/core';
+import { RouterLink, Router} from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
+import { AuthFacade } from '../../../core/facades/auth.facade';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-carrinho',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, PrecoFormatadoPipe],
   templateUrl: './carrinho.html',
   styleUrl: './carrinho.css',
 })
 export class Carrinho {
-  //Lógica do carrinho virá em aulas futuras
-  // por ora, apenas vai exibir a página de carrinho
+   public carrinhoFacade = inject(CarrinhoFacade);
+   private router = inject(Router);
+   private authFacade = inject(AuthFacade);
+
+removerItem (rmvItem: number){
+  this.carrinhoFacade.limparCarrinho();
+
+}
+
+limparCarrinho (){
+  this.carrinhoFacade.limparCarrinho();
+}
+cancelarCompra (){
+this.authFacade.sair();
+this.carrinhoFacade.limparCarrinho();
+this.router.navigateByUrl('/login');
+}
 }
