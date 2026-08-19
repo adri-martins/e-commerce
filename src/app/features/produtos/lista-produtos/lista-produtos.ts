0555e65c55084cf36fcf96a91a7826ecf92b581a
Adrieli Martins <martinsadrieli501@gmail.com>
2026-08-19 13:32:09 -0300

    feat(lista-produtos.ts): Cria novo import e funcionalidade

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -8,6 +8,7 @@ import { inject } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
+import { ItemCarrinho } from '../../../core/models/item-carrinho';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -101,7 +102,7 @@ substituirProduto() {
  
  produtoSelecionado = signal <string | null > (null);
  
- adicionarAoCarrinho(produto: {nome: string; preco: number}){
+ adicionarAoCarrinho(produto:ItemCarrinho){
     this.carrinhoFacade.adiconarProdutoCarrinho(produto);
   }
 
