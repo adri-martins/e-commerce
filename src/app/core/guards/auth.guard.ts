1f093932ea824c6819a4253a413825c7b2c0ae46
Adrieli Martins <martinsadrieli501@gmail.com>
2026-08-19 15:44:53 -0300

    feat(guards): cria nova pasta e adiciona arquivos guard.ts

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { CanActivateFn } from "@angular/router";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthFacade } from "../facades/auth.facade";
+
+
+export const authGuard: CanActivateFn = () => {
+   const router= inject(Router);
+   const authFacade = inject(AuthFacade);
+
+   if(authFacade.usuarioLogado()){
+      return true;
+   }
+   return router.createUrlTree(['/login']);
+   
+}
