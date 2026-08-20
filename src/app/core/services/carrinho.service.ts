4ce334ba7365b75bc6956a33cc58433bf12be629
Adrieli Martins <martinsadrieli501@gmail.com>
2026-08-20 14:06:10 -0300

    chore(carrinho.serive.ts): autilização no código

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -1,87 +1,65 @@
-import { Injectable } from "@angular/core";
-import { signal } from "@angular/core";
-import { computed } from "@angular/core";
-import { ItemCarrinho } from "../models/item-carrinho";
-import { effect } from "@angular/core";
-import { inject } from "@angular/core";
-import { PLATFORM_ID } from "@angular/core";
-import { isPlatformBrowser } from "@angular/common";
+import { isPlatformBrowser } from '@angular/common';
+import { Injectable, signal, computed, effect, inject, PLATFORM_ID } from '@angular/core';
 
+import { ItemCarrinho } from '../models/item-carrinho';
 
 @Injectable({
-    providedIn:'root'
+  providedIn: 'root',
 })
-
 export class CarrinhoService {
+  private platformId = inject(PLATFORM_ID);
+  private readonly chaveStorage = 'minha-loja-carrinho';
+  private carrinho = signal<ItemCarrinho[]>(this.carregarCarrinhoSalvo());
 
-private carrinho = signal<ItemCarrinho[]>(this.carregarcarrinhoSalvo());
+  // SELECTORS
+  itens = computed(() => this.carrinho());
+  quantidadeItens = computed(() => this.carrinho().length);
+  totalIntens = computed(() => this.carrinho().reduce((total, item) => total + item.preco, 0));
+  carrinhoVazio = computed(() => this.carrinho().length === 0);
 
-//? Seletores
-itens = computed(() => this.carrinho());
-quantidadeItens= computed(() => this.carrinho().length); //!Quantidade de itens no carrinho
-totalItens = computed (() =>
-    this.carrinho().reduce((total, item) => total + item.preco,0)
-);
-carrinhoVazio = computed(() => this.carrinho().length === 0); //verificar se o carrinho está vazio
-
-//! ========== PERSISTÊNCIA CARRINHO
-
- private platformId = inject(PLATFORM_ID);
-
- //! Chave de recuperação Localstorage
- private readonly chavesStorage = 'carrinho-storage';
-constructor(){
-    effect(() =>{
-    this.salvarCarrinho(this.carrinho());
+  constructor() {
+    // Sempre que o carrinho mudar, a lista atualizada será persistida.
+    effect(() => {
+      this.salvarCarrinho(this.carrinho());
     });
-}
+  }
 
- private estaNoNavegador(): boolean{
-    return isPlatformBrowser(this.platformId);
- }
+  // ACTIONS
+  adicionar(produto: ItemCarrinho) {
+    this.carrinho.update((lista) => [...lista, produto]);
+  }
 
- private carregarcarrinhoSalvo(): ItemCarrinho [] {
-    if(!this.estaNoNavegador()){
-        return[];
-    }
+  removerItem(rmvItem: number) {
+    this.carrinho.update((listaAtual) => listaAtual.filter((_, index) => index !== rmvItem));
+  }
 
-    const dadosSalvos = localStorage.getItem(this.chavesStorage);
-
-    if(!dadosSalvos){
-        return [];
-    }
-
-    try{
-        return JSON.parse(dadosSalvos) as ItemCarrinho[];
-    } catch {
-        return[];
-    }
- }
-  
- private salvarCarrinho (item: ItemCarrinho[]){
-    if(!this.estaNoNavegador()){
-        return;
-    }
-
-    localStorage.setItem(this.chavesStorage, JSON.stringify(item));
-    
- }
-
-// TODO: Ações
-adicionar(produto: ItemCarrinho){
-    this.carrinho.update(lista => [ ...lista, produto]);
-}
-
-
-limpar() {
+  limpar() {
     this.carrinho.set([]);
-}
+  }
 
+  private estaNoNavegador(): boolean {
+    return isPlatformBrowser(this.platformId);
+  }
 
-removerItem(rmvItem:number){
-    this.carrinho.update((listaAtual)=>
-    listaAtual.filter((_, index) => index !== rmvItem));
-    
-}
+  private carregarCarrinhoSalvo(): ItemCarrinho[] {
+    if (!this.estaNoNavegador()) {
+      return [];
+    }
+    const dadosSalvos = localStorage.getItem(this.chaveStorage);
+    if (!dadosSalvos) {
+      return [];
+    }
+    try {
+      return JSON.parse(dadosSalvos) as ItemCarrinho[];
+    } catch {
+      return [];
+    }
+  }
 
+  private salvarCarrinho(itens: ItemCarrinho[]) {
+    if (!this.estaNoNavegador()) {
+      return;
+    }
+    localStorage.setItem(this.chaveStorage, JSON.stringify(itens));
+  }
 }
