0c2f73be484c8e68ff8d84ea340829a7f8d36e47
Adrieli Martins <martinsadrieli501@gmail.com>
2026-08-20 13:39:04 -0300

    feat(admin.guard.ts): adiciona import

--- src/app/core/guards/admin.guard.ts
+++ src/app/core/guards/admin.guard.ts
@@ -1,5 +1,4 @@
 import { inject } from "@angular/core";
-
 import { Router } from "@angular/router";
 import { CanActivateFn } from "@angular/router";
 import { AuthService } from "../services/auth.service";
