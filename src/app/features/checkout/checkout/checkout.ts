6e3639510217cf3d658b31604940d1e2bb25c3a7
Adrieli Martins <martinsadrieli501@gmail.com>
2026-08-21 17:06:50 -0300

    refactor(checkout.ts): ajeita o código

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -15,6 +15,7 @@ import { AuthService } from '../../../core/services/auth.service';
 import { AuthFacade } from '../../../core/facades/auth.facade';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { ItemCarrinho } from '../../../core/models/item-carrinho';
+import { MatButtonModule } from '@angular/material/button';
 
 
 type pedidoFinalizado = {
@@ -27,7 +28,7 @@ type pedidoFinalizado = {
 
 @Component({
   selector: 'app-checkout',
-  imports: [ReactiveFormsModule, RouterLink, PrecoFormatadoPipe],
+  imports: [ReactiveFormsModule, RouterLink, PrecoFormatadoPipe, MatButtonModule],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
